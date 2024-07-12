681e3003c7745b6ce93348b5f87d2ff587d44435
Sanu Kumar <sanu23kumar@gmail.com>
2024-07-13 03:11:28 +0530

    remove: user info from expenses

--- app/expenses/page.tsx
+++ app/expenses/page.tsx
@@ -6,9 +6,6 @@ import Savings from "@components/UserFinancialActivities";
 function Home() {
   return (
     <section className="grid sm:grid-cols-2 h-[80vh]">
-      <div className="flex sm:items-center w-3/4 sm:m-auto">
-        <UserInfo />
-      </div>
       <div className="sm:overflow-y-auto">
         <AddSaving />
         <Savings />
