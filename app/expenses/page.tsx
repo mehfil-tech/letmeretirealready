7beac640b10c2adc3082cfacce07c98f2068f723
Sanu Kumar <sanu23kumar@gmail.com>
2024-07-17 02:19:28 +0530

    fix style expenses

--- app/expenses/page.tsx
+++ app/expenses/page.tsx
@@ -6,9 +6,14 @@ import useFirebaseHooks from "@services/useFirebaseHooks";
 import { ExpenseCategory } from "@models/ExpenseCategory";
 import CategorySelecter from "./CategorySelecter";
 import expenseHelper from "./expenseHelper";
+import MonthSelector from "./MonthSelector";
 
 function Home() {
-  const { expenses, addExpense, deleteExpense } = useExpenseStore();
+  const {
+    data: expenses,
+    addData: addExpense,
+    deleteData: deleteExpense,
+  } = useFirebaseHooks<Expense>("expenses");
   const { data: categories } =
     useFirebaseHooks<ExpenseCategory>("expenseCategories");
   const onSubmit = (values: Expense) => {
@@ -19,21 +24,26 @@ function Home() {
     categories
   );
   return (
-    <section className="flex flex-row">
-      <ExpenseTable
-        expenses={expenses}
-        categories={categories}
-        deleteExpense={deleteExpense}
-        onSubmit={onSubmit}
-      />
-      <div>
-        <CategorySelecter
-          categories={orderedCategories}
-          totalExpense={totalExpense}
-          onSelect={(category) => console.log(category)}
+    <div className="flex flex-col flex-1">
+      <section className="flex flex-row flex-1">
+        <ExpenseTable
+          expenses={expenses}
+          categories={categories}
+          deleteExpense={deleteExpense}
+          onSubmit={onSubmit}
         />
+        <div>
+          <CategorySelecter
+            categories={orderedCategories}
+            totalExpense={totalExpense}
+            onSelect={(category) => console.log(category)}
+          />
+        </div>
+      </section>
+      <div>
+        <MonthSelector onSelect={(month) => console.log(month)} />
       </div>
-    </section>
+    </div>
   );
 }
 
